023a670e93a8e0d409234e81fadfe6e21e8a9868
Xu Jingxin <xjx209378@alibaba-inc.com>
2020-08-04 16:28:30 +0800

    Add chinese test data

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+BERT_DIR=bleurt/zh_checkpoint
+BERT_CKPT=multi_cased_L-12_H-768_A-12/bert_model.ckpt
+python3 -m bleurt.finetune \
+  -init_checkpoint=${BERT_DIR}/${BERT_CKPT} \
+  -bert_config_file=${BERT_DIR}/bert_config.json \
+  -vocab_file=${BERT_DIR}/vocab.txt \
+  -model_dir=my_new_bleurt_checkpoint \
+  -train_set=bleurt/test_data/ratings_train.jsonl \
+  -dev_set=bleurt/test_data/ratings_dev.jsonl \
+  -num_train_steps=500
