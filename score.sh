857a8f8440c3ae8467b8ec95f1ce2ef4efcc3a77
Xu Jingxin <xjx209378@alibaba-inc.com>
2020-08-05 15:55:45 +0800

    Add score script

---
+++ score.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python -m bleurt.score \
+  -candidate_file=bleurt/test_data/candidates \
+  -reference_file=bleurt/test_data/references \
+  -bleurt_checkpoint=my_new_bleurt_checkpoint/export/bleurt_best/1596535070
