5925493ff6f8f7a357a8adac3f009d07788b41da
Xu Jingxin <sailxjx@gmail.com>
2020-09-08 14:51:07 +0800

    Add rest TODO

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-BERT_DIR=bleurt/zh_checkpoint
-BERT_CKPT=multi_cased_L-12_H-768_A-12/bert_model.ckpt
+BERT_DIR=bleurt/zh_checkpoint/chinese_L-12_H-768_A-12
+BERT_CKPT=my_new_bleurt_checkpoint/model.ckpt-1210000
 python3 -m bleurt.finetune \
-  -init_checkpoint=${BERT_DIR}/${BERT_CKPT} \
+  -init_checkpoint=${BERT_CKPT} \
   -bert_config_file=${BERT_DIR}/bert_config.json \
   -vocab_file=${BERT_DIR}/vocab.txt \
   -model_dir=my_new_bleurt_checkpoint \
